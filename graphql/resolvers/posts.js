18cf37a574694c5007c40d644eddbfb6815898e7
Monaam12 <clemonaam@gmail.com>
2020-10-26 10:25:16 +0100

    initial commit

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,12 @@
 const Post = require("../../Models/Post");
+const { AuthenticationError } = require("apollo-server");
 const checkAuth = require("../../utilites/checkAuth");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (error) {
         throw new Error(error);
@@ -20,24 +21,37 @@ module.exports = {
         } else {
           throw new Error("Post not found");
         }
-      }catch(err){
+      } catch (err) {
         throw new Error(err);
       }
     },
   },
   Mutation: {
-    async createPost(_, {body}, context){
+    async createPost(_, { body }, context) {
       const user = checkAuth(context);
-console.log(user);
       const newPost = new Post({
         body,
         user: user.id,
         username: user.username,
-        createdAt: new Date().toISOString()
-      })
+        createdAt: new Date().toISOString(),
+      });
 
       const post = await newPost.save();
       return post;
-    }
-  }
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.deleteOne();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
 };
