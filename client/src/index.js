6817564760aa83f96d232908a96dfb01037d7372
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-02-26 13:28:18 +0100

    FEAT | add parcel & fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,4 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import ApolloProvider from './ApolloProvider';
+import ReactDOM from "react-dom";
+import ApolloProvider from "./ApolloProvider";
 
-ReactDOM.render(
-  ApolloProvider,
-  document.getElementById('root')
-);
-
-
-reportWebVitals();
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
