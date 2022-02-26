ebb0a3652f346c33fd3bbe0204f28199a6c776a1
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-02-26 11:28:35 +0100

    add server folder

---
+++ server/graphql/resolvers/users.js
@@ -0,0 +1,96 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+const User = require("../Models/User");
+const { SECRET_KEY } = require("../../../config");
+const {
+  validateRegisterInput,
+  validateloginInput,
+} = require("../utilites/validator");
+
+function generateToken(user) {
+  jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
+}
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateloginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config.js");
+const typeDefs = require("./server/graphql/TypeDefs");
+const resolvers = require("./server/graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`🚀  Server ready at ${res.url}`);
+  });
